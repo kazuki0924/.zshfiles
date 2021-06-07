fadc6b210d389e16b2a95d9587d01f92d3ff5053
Kazuki M <kazuki0924@gmail.com>
2021-06-07 19:10:02 +0900

    fix tmux

--- after_after/tmux.zsh
+++ after_after/tmux.zsh
@@ -1,7 +1,10 @@
 export TMUX_CLIENT="alacritty"
 
 # attach existing or start new tmux session on start for the first instance
-if [[ -z "$TMUX" ]] && [[ ! $(tmux list-clients | rg "$TMUX_CLIENT") ]] &> /dev/null; then
-  tmux attach &> /dev/null || tmux &> /dev/null
+if [[ -z "$TMUX" ]]; then
+  if [[ $(tmux list-clients | wc -l) -ge 1 ]]; then
+    tmux new-session
+  else
+    tmux new-session -A -s main
+  fi
 fi
-
