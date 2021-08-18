b904990b6206da611a8ca047bcf941940646b7ae
Kazuki M <kazuki0924@gmail.com>
2021-08-18 14:04:51 +0900

    Update: tmux.zsh

--- before_before/tmux.zsh
+++ before_before/tmux.zsh
@@ -1,10 +1,16 @@
 export TMUX_CLIENT="alacritty"
 
-# attach existing or start new tmux session on start for the first instance
-if [[ -z "$TMUX" ]]; then
-  if [[ $(tmux list-clients | wc -l) -ge 1 ]]; then
-    tmux new-session
-  else
-    tmux new-session -A -s main
-  fi
+TERMINALS_TO_EXCLUDE=(
+	Apple_Terminal
+)
+
+if [[ ! " ${TERMINALS_TO_EXCLUDE[@]} " =~ " ${TERM_PROGRAM} " ]]; then
+	# attach existing or start new tmux session on start for the first instance
+	if [[ -z "$TMUX" ]]; then
+		if [[ $(tmux list-clients | wc -l) -ge 1 ]]; then
+			tmux new-session
+		else
+			tmux new-session -A -s main
+		fi
+	fi
 fi
