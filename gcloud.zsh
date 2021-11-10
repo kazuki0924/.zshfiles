1d6cd69440afd7c206d794ad7cfced370feae097
Kazuki M <kazuki0924@gmail.com>
2021-11-10 12:28:25 +0900

    Fix: gcloud

--- gcloud.zsh
+++ gcloud.zsh
@@ -1,2 +1,4 @@
-source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
-source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+if which gcloud &>/dev/null; then
+  source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+  source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+fi
