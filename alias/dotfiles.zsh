42a03992b51450950395f5ebe1da6e31fb3ec35f
Kazuki M <kazuki0924@gmail.com>
2021-11-18 01:37:31 +0900

    fix: exa

--- alias/dotfiles.zsh
+++ alias/dotfiles.zsh
@@ -1,8 +1,8 @@
 # dotfiles
 alias dotsync="cd $HOME/.dotfiles && make"
-alias zdf="cd $HOME/.dotfiles && clear && exa -l --icons"
-alias zds="cd $HOME/.dotscripts && clear && exa -l --icons"
-alias zdn="cd $HOME/.nvimfiles && clear && exa -l --icons"
-alias zdz="cd $HOME/.zshfiles && clear &&exa -l --icons" 
+alias zdf="cd $HOME/.dotfiles && clear && exa -la --icons"
+alias zds="cd $HOME/.dotscripts && clear && exa -la --icons"
+alias zdn="cd $HOME/.nvimfiles && clear && exa -la --icons"
+alias zdz="cd $HOME/.zshfiles && clear &&exa -la --icons" 
 
 alias lc="log_collector.sh"
