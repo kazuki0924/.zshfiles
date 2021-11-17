f6b90e759eaade5990bdf644007e2a8c5559758f
Kazuki M <kazuki0924@gmail.com>
2021-11-18 01:19:18 +0900

    Add: exa

--- alias/dotfiles.zsh
+++ alias/dotfiles.zsh
@@ -1,8 +1,8 @@
 # dotfiles
 alias dotsync="cd $HOME/.dotfiles && make"
-alias zdf="cd $HOME/.dotfiles && clear && lsd -1 -a"
-alias zds="cd $HOME/.dotscripts && clear && lsd -1 -a"
-alias zdn="cd $HOME/.nvimfiles && clear && lsd -1 -a"
-alias zdz="cd $HOME/.zshfiles && clear && lsd -1 -a"
+alias zdf="cd $HOME/.dotfiles && clear && exa -l --icons"
+alias zds="cd $HOME/.dotscripts && clear && exa -l --icons"
+alias zdn="cd $HOME/.nvimfiles && clear && exa -l --icons"
+alias zdz="cd $HOME/.zshfiles && clear &&exa -l --icons" 
 
 alias lc="log_collector.sh"
--- alias/ls.zsh
+++ alias/ls.zsh
@@ -1,12 +1,7 @@
-# lsd
-alias l="lsd -1 -a -l"
-
 # exa
-alias e='exa'                                                          # ls
-alias el='exa -lbF --git'                                              # list, size, type, git
-alias ell='exa -lbGF --git'                                            # long list
-alias ellm='exa -lbGd --git --sort=modified'                           # long list, modified date sort
-alias ela='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale' # all list
-alias ex='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
-alias elS='exa -1'                                                     # one column, just names
-alias elt='exa --tree --level=2'                                       # tree
+alias l='exa -lbF --icons --git'                                              # list, size, type, git
+alias ll='exa -lbGF --icons --git'                                            # long list
+alias llm='exa -lbGd --icons --git --sort=modified'                           # long list, modified date sort
+alias lll='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --icons' # all + extended list
+alias l1='exa -1'                                                     # one column, just names
+alias lt='exa --tree --icons'                                       # tree
