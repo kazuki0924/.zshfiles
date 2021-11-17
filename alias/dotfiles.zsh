f02b1d03425a35acfaec5276c7e101191ce03caa
Kazuki M <kazuki0924@gmail.com>
2021-11-17 12:52:08 +0900

    Add: alias

--- alias/dotfiles.zsh
+++ alias/dotfiles.zsh
@@ -4,3 +4,5 @@ alias zdf="cd $HOME/.dotfiles && clear && lsd -1 -a"
 alias zds="cd $HOME/.dotscripts && clear && lsd -1 -a"
 alias zdn="cd $HOME/.nvimfiles && clear && lsd -1 -a"
 alias zdz="cd $HOME/.zshfiles && clear && lsd -1 -a"
+
+alias lc="log_collector.sh"
