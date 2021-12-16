9ccaa9850ab53440ebd77843aa9170f3c72b2857
Kazuki M <kazuki0924@gmail.com>
2021-12-16 12:36:33 +0900

    fix

--- path/usr_local_bin_to_first.zsh
+++ path/usr_local_bin_to_first.zsh
@@ -1,4 +1,4 @@
 # PATH
-export PATH="/.local/bin:${PATH//:\/bin//}:/bin"
+export PATH="${HOME}/.local/bin:${PATH//:\/bin//}:/bin"
 export PATH="/usr/local/sbin:${PATH//:\/bin//}:/bin"
 export PATH="/usr/local/bin:${PATH//:\/bin//}:/bin"
